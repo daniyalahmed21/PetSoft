d63654be81c3eeb45edbbc3dde09ae29cae51692
Daniyal Ahmed <da9033431@gmail.com>
2025-02-11 21:54:38 +0500

    revalidation

--- src/app/actions/actions.ts
+++ src/app/actions/actions.ts
@@ -4,10 +4,48 @@ import { prisma } from "@/lib/db";
 import { Pet } from "@/lib/types";
 import { revalidatePath } from "next/cache";
 
-export async function AddPet(pet:Pet) {
-  await prisma.pet.create({
-    data: pet,
-  });
+export async function AddPet(pet: Pet) {
+  try {
+    await prisma.pet.create({
+      data: pet,
+    });
 
-  revalidatePath('/app','layout');
+    revalidatePath("/app", "layout");
+  } catch (error) {
+    return {
+      msg: "Error creating pet ",
+    };
+  }
+}
+
+export async function editPet(selectedPetId: string, updatedPet: Pet) {
+  try {
+    await prisma.pet.update({
+      where: {
+        id: selectedPetId,
+      },
+      data: updatedPet,
+    });
+    revalidatePath("/app", "layout");
+  } catch (error) {
+    return {
+      msg: "Error checking out pet ",
+    };
+  }
+}
+
+
+export async function checkoutPet(selectedPetId: string) {
+  try {
+    await prisma.pet.delete({
+      where: {
+        id: selectedPetId,
+      }
+    });
+    revalidatePath("/app", "layout");
+  } catch (error) {
+    return {
+      msg: "Error editing pet ",
+    };
+  }
 }
