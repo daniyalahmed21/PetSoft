ed62b66054f0ff2c0f9360eae7c16d47172dad0b
Daniyal Ahmed <da9033431@gmail.com>
2025-02-14 23:02:26 +0500

    Authentication

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,8 +1,18 @@
+import AuthForm from '@/components/AuthForm'
+import H1 from '@/components/H1'
+import Link from 'next/link'
 import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login Page</div>
+<main >
+  <H1 className='text-center mb-5'>Login Page</H1>
+    <AuthForm type="login"/>
+    <p className='text-sm mt-4 text-zinc-500' >
+      No account yet? {" "}
+      <Link className='text-medium ' href='/signup'>Sign up</Link> 
+    </p>
+</main>
   )
 }
 
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import AuthForm from "@/components/AuthForm";
+import H1 from "@/components/H1";
+import Link from "next/link";
+import React from "react";
 
 function Signup() {
   return (
-    <div>Signup Page</div>
-  )
+    <main>
+      <H1 className="text-center mb-5">Signup Page</H1>
+      <AuthForm type="signup"/>
+      <p className="text-sm mt-4 text-zinc-500">
+        Already have an account?{" "}
+        <Link className="text-medium " href="/login">
+          Sign up
+        </Link>
+      </p>
+    </main>
+  );
 }
 
-export default Signup
+export default Signup;
